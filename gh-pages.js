918955e681c93ad0936ca079b0bc2e7a1cfc723e
Enno Hermann <enno.hermann@gmail.com>
2023-11-23 23:15:28 +0100

    build: remove gh-pages default options

--- gh-pages.js
+++ gh-pages.js
@@ -1,16 +1,7 @@
 var ghpages = require('gh-pages');
 
 ghpages.publish(
-	'public', // path to public directory
-	{
-		branch: 'gh-pages',
-		repo: 'https://github.com/eginhard/gaussian-distributions.git', // Update to point to your repository
-		user: {
-			name: 'Enno Hermann', // update to use your name
-			email: 'enno.hermann@gmail.com' // Update to use your email
-		},
-		dotfiles: true
-	},
+	'public',
 	() => {
 		console.log('Deploy Complete!');
 	}
